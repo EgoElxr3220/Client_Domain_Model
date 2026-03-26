4758d316de83daea45befaab705650e1470917b5
Lance Boylan <eg01st3220@gmail.com>
2026-03-26 16:11:06 -0600

    Add project files.

---
+++ Client_Domain_Model/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManagement.Models
+{
+    internal class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+        private bool IsActive { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+        public Client(Guid id, string name, string email, DateTime createdOn, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedOn = createdOn;
+            IsActive = isActive;
+        }   
+
+        public string GetSummary()
+        {
+            string active;
+            if (IsActive)
+            {
+                active = "Active";
+            }
+            else
+            {
+                active = "Inactive";
+            }
+            return $"Client: {Name}\nEmail: {Email}\nStatus: {active}\nCreated: {CreatedOn}";
+        }
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+    }
+}
---
+++ Client_Domain_Model/Program.cs
@@ -0,0 +1,16 @@
+﻿using CompanyManagement.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Client client1 = new Client("SnowTech Solutions", "info@snowtech.com");
+        string client1Summary = client1.GetSummary();
+        Console.WriteLine(client1Summary);
+
+        Client client2 = new Client("Ego's Elixirs", "info@egoselixirs.com");
+        client2.Deactivate();
+        string client2Summary = client2.GetSummary();
+        Console.WriteLine(client2Summary);
+    }
+}
